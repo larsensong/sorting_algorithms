2062707797aa94e915792de63e0a8ebbc75dd4da
larsen_dev <53650985+larsensong@users.noreply.github.com>
2023-04-29 23:46:41 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -50,7 +50,7 @@ typedef enum bool
  *
  *     * @prev: Pointer to the previous element of the list
  *
- *      * @next: Pointer to the next data  element
+ *      * @next: Pointer to the next data  element.
 
  element of the list. 
  *
